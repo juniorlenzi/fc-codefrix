5811b5ab6ffbece0ce385ee487c905af8ebc7339
Junior Lenzi <junior.lenzi@outlook.com>
2024-06-01 12:20:06 +0000

    category validate tests

--- src/domain/__tests__/category.entity.spec.ts
+++ src/domain/__tests__/category.entity.spec.ts
@@ -1,38 +1,45 @@
+import { UUID } from '../../@shared/domain/value-objects/uuid.vo'
 import { Category } from '../category.entity'
 
 describe('Category Unit Tests', () => {
+  let validateSpy: any
+  beforeEach(() => {
+    validateSpy = jest.spyOn(Category, 'validate')
+  })
+
   test('constructor', () => {
     let category = new Category({
       name: 'Movie'
     })
 
-    expect(category.category_id).toBeUndefined()
+    expect(category.category_id).toBeInstanceOf(UUID)
     expect(category.name).toBe('Movie')
     expect(category.description).toBeNull()
     expect(category.is_active).toBe(true)
     expect(category.created_at).toBeInstanceOf(Date)
   })
   test('should create a category using static create method', () => {
-    const createdAt = new Date()
     const category = Category.create({
       name: 'Movie',
       description: 'Movie Description',
       is_active: false
     })
 
-    expect(category.category_id).toBeUndefined()
+    expect(category.category_id).toBeInstanceOf(UUID)
     expect(category.name).toBe('Movie')
     expect(category.description).toBe('Movie Description')
     expect(category.is_active).toBe(false)
+    expect(validateSpy).toHaveBeenCalledTimes(1)
   })
 
   test('should change name', () => {
-    const category = new Category({
+    const category = Category.create({
       name: 'Movie'
     })
 
     category.changeName('Movies')
     expect(category.name).toBe('Movies')
+    expect(validateSpy).toHaveBeenCalledTimes(2)
   })
 
   test('should change description', () => {
@@ -64,7 +71,9 @@ describe('Category Unit Tests', () => {
   })
 
   test('should convert to JSON', () => {
+    const id = new UUID()
     const category = new Category({
+      category_id: id,
       name: 'Movie',
       description: 'Movie Description',
       is_active: false
@@ -73,11 +82,28 @@ describe('Category Unit Tests', () => {
     const json = category.toJSON()
 
     expect(json).toEqual({
-      category_id: undefined,
+      category_id: id.id,
       name: 'Movie',
       description: 'Movie Description',
       is_active: false,
       created_at: expect.any(Date)
+    })
+  })
+
+  describe('category_id field', () => {
+    const arrange = [{ category_id: null }, { category_id: undefined }, { category_id: new UUID() }]
+
+    test.each(arrange)('id = %j', ({ category_id }) => {
+      const category = new Category({
+        name: 'Movie',
+        category_id: category_id as any
+      })
+
+      expect(category.category_id).toBeInstanceOf(UUID)
+
+      if (category_id instanceof UUID) {
+        expect(category.category_id).toBe(category_id)
+      }
     })
   })
 })
--- src/domain/category.entity.ts
+++ src/domain/category.entity.ts
@@ -1,5 +1,8 @@
+import { UUID } from '../@shared/domain/value-objects/uuid.vo'
+import { CategoryValidatorFactory } from './category.validator'
+
 export type CategoryConstructorProps = {
-  category_id?: string
+  category_id?: UUID
   name: string
   description?: string | null
   is_active?: boolean
@@ -13,29 +16,33 @@ export type CategoryCreateCommand = {
 
 export class Category {
   constructor(props: CategoryConstructorProps) {
-    this.category_id = props.category_id
+    this.category_id = props.category_id ?? new UUID()
     this.name = props.name
     this.description = props.description ?? null
     this.is_active = props.is_active ?? true
     this.created_at = props.created_at ?? new Date()
   }
 
-  category_id: string | undefined
+  category_id: UUID
   name: string
   description: string | null
   is_active: boolean
   created_at: Date
 
   static create(props: CategoryCreateCommand): Category {
-    return new Category(props)
+    const category = new Category(props)
+    Category.validate(category)
+    return category
   }
 
   changeName(name: string): void {
     this.name = name
+    Category.validate(this)
   }
 
   changeDescription(description: string | null): void {
     this.description = description
+    Category.validate(this)
   }
 
   activate(): void {
@@ -46,9 +53,15 @@ export class Category {
     this.is_active = false
   }
 
+  static validate(entity: Category) {
+    const validator = CategoryValidatorFactory.create()
+
+    return validator.validate(entity)
+  }
+
   toJSON(): object {
     return {
-      category_id: this.category_id,
+      category_id: this.category_id.id,
       name: this.name,
       description: this.description,
       is_active: this.is_active,
